48cb40efebe1c9b1cee5cdb396a1444fadf3f0bd
Malhar <malhardevasthali007@gmail.com>
2024-07-30 19:17:15 +0530

    Added LEvel Data

--- Array-Jumper/source/Level/LevelModel.cpp
+++ Array-Jumper/source/Level/LevelModel.cpp
@@ -1,4 +1,5 @@
 #include "../../header/Level/LevelModel.h"
+using namespace Level;
 
 Level::LevelModel::LevelModel()
 {
@@ -10,4 +11,9 @@ Level::LevelModel::~LevelModel()
 
 void Level::LevelModel::initialize()
 {
+}
+
+BlockType Level::LevelModel::getCurrentBoxValue(int currentPosition)
+{
+	return current_level_data.level_boxes[currentPosition];
 }
