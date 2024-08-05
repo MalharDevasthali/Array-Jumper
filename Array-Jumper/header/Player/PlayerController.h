1d967559129a507b84850f5cd498f0f77e897651
Malhar <malhardevasthali007@gmail.com>
2024-08-05 09:03:40 +0530

    Added Player Movement

--- Array-Jumper/header/Player/PlayerController.h
+++ Array-Jumper/header/Player/PlayerController.h
@@ -1,16 +1,22 @@
 #pragma once
-
+#include "MovementDirection.h"
+#include "../../header/Event/EventService.h"
 namespace Player
 {
 	class PlayerView;
 	class PlayerModel;
 	enum class PlayerState;
+
 	class PlayerController
 	{
 	private:
 		PlayerModel* player_model;
 		PlayerView* player_view;
+		Event::EventService* event_service;
 
+		void move(MovementDirection direction);
+		void readInput();
+		bool isPositionInBound(int targetPosition);
 		void destroy();
 
 	public:
--- Array-Jumper/source/Player/PlayerController.cpp
+++ Array-Jumper/source/Player/PlayerController.cpp
@@ -1,9 +1,13 @@
 #include "../../header/Player/PlayerController.h"
 #include "../../header/Player/PlayerView.h"
 #include "../../header/Player/PlayerModel.h"
+#include "../../header/Global/ServiceLocator.h"
 
 namespace Player
 {
+	using namespace Level;
+	using namespace Global;
+	using namespace Sound;
 	PlayerController::PlayerController()
 	{
 		player_model = new PlayerModel();
@@ -16,16 +20,19 @@ namespace Player
 	{
 		player_model->initialize();
 		player_view->initialize();
+		event_service = ServiceLocator::getInstance()->getEventService();
 	}
 
 	void PlayerController::update()
 	{
 		player_view->update();
+		readInput();
 	}
 
 	void PlayerController::render()
 	{
 		player_view->render();
+		
 	}
 
 	PlayerState PlayerController::getPlayerState()
@@ -41,6 +48,50 @@ namespace Player
 	int PlayerController::getCurrentPosition()
 	{
 		return player_model->getCurrentPosition();
+	}
+
+	void PlayerController::move(MovementDirection direction)
+	{
+		int steps, targetPosition;
+		switch (direction)
+		{
+		case MovementDirection::FORWARD:
+			steps = 1;
+			break;
+		case MovementDirection::BACKWARD:
+			steps = -1;
+			break;
+		default:
+			steps = 0;
+			break;
+		}
+
+		targetPosition = player_model->getCurrentPosition() + steps;
+
+		if (!isPositionInBound(targetPosition))
+			return;
+
+		player_model->setCurrentPosition(targetPosition);
+		ServiceLocator::getInstance()->getSoundService()->playSound(SoundType::MOVE);
+	}
+
+	void PlayerController::readInput()
+	{
+		if (event_service->pressedRightArrowKey() || event_service->pressedDKey())
+		{
+			move(MovementDirection::FORWARD);
+		}
+		if (event_service->pressedLeftArrowKey() || event_service->pressedAKey())
+		{
+			move(MovementDirection::BACKWARD);
+		}
+	}
+
+	bool PlayerController::isPositionInBound(int targetPosition)
+	{
+		if (targetPosition >= 0 && targetPosition < LevelData::NUMBER_OF_BOXES)
+			return true;
+		return false;
 	}
 
 	void PlayerController::destroy()
